fec62b32d76225868322062ea7539c0b5a1fcb43
vavillanueva <valentinvillanueva506@gmail.com>
2024-09-10 15:42:56 -0300

    verificacion de numeros

--- vavillanueva/tablademultiplicar.cpp
+++ vavillanueva/tablademultiplicar.cpp
@@ -6,8 +6,12 @@ int main() {
     int numero;
     cout<< "ingrese un numero:"<< endl;
     cin>> numero;
+    if(numero <= 0){
+        cout << "ingrese un numero mayor a 0.";
+}
+    else{
 
     for (int i = 1; i<= 10; i++){
         cout << numero <<"x" <<i << "=" <<numero * i << endl;
-    }
+}    }
 }
